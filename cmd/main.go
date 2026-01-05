8f743f0116ae5ff7f12b67044ef064e55203e5c3
AARON ISRAEL <htlgil41@gmail.com>
2026-01-05 13:09:03 -0400

    Integracoin del modulo para generar la llave y crear el archivo

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,15 @@ package cmd
 
 import (
 	"fmt"
+	"nafiles/fs"
 	"os"
 	"time"
 )
 
 func InitToolsForNa() {
 
-	var directorioNa string = "na_files"
+	var directorioNa string = fmt.Sprintf("./%s", "na_files")
+	var aeskey string = "AES.key"
 	var opc int32
 	fmt.Println("Debe seguir cada paso como se explica para lograr encriptar de forma segura y eficiente el archivo deseado")
 	fmt.Printf("Se creara un directorio llamado %q en el path de la aplicacion\n", directorioNa)
@@ -28,7 +30,7 @@ chooseOpc:
 
 		case 1:
 			{
-				os.Mkdir(fmt.Sprintf("./%s", directorioNa), 0750)
+				os.Mkdir(directorioNa, 0750)
 				fmt.Println("Se creo el directorio correctamente")
 				fmt.Println("..............")
 
@@ -49,6 +51,68 @@ chooseOpc:
 				fmt.Println("..............")
 				continue chooseOpc
 			}
+		}
+	}
+
+	fmt.Println("Ahora debera seleccionar las siguientes opcion para la accion")
+	fmt.Println("la opcion que ingresara es invalida el programa se cerrara")
+
+	fmt.Printf("1. Tengo una llave AES\n2. Crear llave AES\n")
+	_, errScanOpt := fmt.Scanf("%d", &opc)
+	if errScanOpt != nil {
+
+		fmt.Printf("Valor invalido el programa se cerrara")
+		os.Exit(0)
+
+		return
+	}
+
+	switch opc {
+
+	case 1:
+		{
+			fmt.Println("Usted posee una llave AES que debera moverla en el direcotorio creado")
+			fmt.Printf("Mueva la llave al directorio %q (el archivo debe ser llamado %q)\n", directorioNa, aeskey)
+
+			fmt.Println("Presione cualquier tecla valida si ya movio la llave AES")
+			var anyKey string
+			fmt.Scanf("%s", &anyKey)
+
+			fmt.Println(aeskey)
+		}
+	case 2:
+		{
+
+			fmt.Println("Se creara la llave AES")
+			fmt.Printf("Direcotrio donde estara la llave %q (el archivo se llama %q)\n", directorioNa, aeskey)
+
+			key, errAes := GenerateKeyAESforNcripted()
+			if errAes != nil {
+
+				fmt.Println("error al generar la llave AES ", errAes)
+				os.Exit(0)
+				return
+			}
+
+			fileAes, errFileAes := fs.CreateFile(
+				directorioNa,
+				aeskey,
+			)
+			if errFileAes != nil {
+
+				fmt.Println("Erorr al crear el archivo con la llave")
+				os.Exit(0)
+				return
+			}
+			defer fileAes.Close()
+
+			fileAes.WriteString(key)
+		}
+	default:
+		{
+			fmt.Println("Opcion invalida programa cerrado....")
+			os.Exit(0)
+			return
 		}
 	}
 }
